eab7dd7d898963d4ffe5057e62158e9f7518d7bb
David W <davo49fb@gmail.com>
2022-01-19 17:02:31 -0500

    add to gitignore

--- frontend/src/models.ts
+++ frontend/src/models.ts
@@ -1,17 +1,3 @@
 import { Player } from "./player";
 
-export interface Game {
-  players: Player[];
-  deck: Card[];
-  discard: Card[];
-}
 
-export interface Suit {
-  name: 'hearts' | 'diamonds' | 'spades' | 'clubs';
-  color: 'red' | 'black';
-}
-
-export interface Card {
-  suit: Suit;
-  owner: Player;
-}
