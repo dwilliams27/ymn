7d230defe8e812912c428bbcdaf290d61178ff8a
David W <davo49fb@gmail.com>
2022-01-19 20:14:38 -0500

    improve names

--- backend/utils/NameGen.py
+++ backend/utils/NameGen.py
@@ -1,5 +1,27 @@
 import random
 
+vowels = [
+  'a',
+  'e',
+  'i',
+  'o',
+  'u'
+]
+
+doublers = [
+  'r',
+  't',
+  'p',
+  'd',
+  'g',
+  'l',
+  'z',
+  'c',
+  'b',
+  'n',
+  'm'
+]
+
 fname_pre = [
   'Jim',
   'Bri',
@@ -53,7 +75,10 @@ lname_post = [
   'ock',
   'eemus',
   'ent',
-  'alty'
+  'alty',
+  'ustaus',
+  'umber',
+  'atto'
 ]
 
 superlative = [
@@ -68,14 +93,27 @@ superlative = [
   'the Triumphant',
   'the Elder',
   'the Warrior',
-  'the Younger'
+  'the Younger',
+  'the Traitor',
+  'the Vile',
+  'the Beloved',
+  'the Hated',
+  'the Terrible',
+  'the Diseased',
+  'the Generous',
+  'the Unlucky'
 ]
 
 class NameFactory:
   @staticmethod
   def generateName():
-    name = fname_pre[random.randrange(len(fname_pre))] + fname_post[random.randrange(len(fname_post))] + \
+    fname1 = fname_pre[random.randrange(len(fname_pre))]
+    fname2 = fname_post[random.randrange(len(fname_post))]
+    if(fname1[-1] in doublers and fname2[0] in vowels):
+      fname1 += fname1[-1]
+
+    name = fname1 + fname2 + \
       ' ' + lname_pre[random.randrange(len(lname_pre))] + lname_post[random.randrange(len(lname_post))]
-    if(random.randrange(100) > 90):
+    if(random.randrange(100) > 80):
       name += f' {superlative[random.randrange((len(superlative)))]}'
     return name
